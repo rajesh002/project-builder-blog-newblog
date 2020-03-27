8cb4884143a9ae2f923cc54c3066d114ce2d9cb7
rajesh002 <rajeshbtech16@gmail.com>
2020-03-27 21:21:10 +0530

    project with blog builds completed

--- src/model/Blog.java
+++ src/model/Blog.java
@@ -1,2 +1,36 @@
 package model;
-// Type your code
+
+import java.time.LocalDate;
+
+public class Blog{
+	String title;
+	String description;
+	LocalDate postedOn;
+	public Blog(String title,String description,LocalDate postedOn){
+		this.title=title;
+		this.description=description;
+		this.postedOn=postedOn;
+	}
+	public void setBlogDescription(String description) {
+		this.description=description;
+	}
+	
+	public void setBlogTitle(String title) {
+		this.title=title;
+	}
+	public void setDate(LocalDate postedOn) {
+		this.postedOn=postedOn;
+	}
+	
+	
+	public String getBlogDescription() {
+		return description;
+	}
+	public String getBlogTitle() {
+		return title;
+	}
+	
+	public LocalDate getDate() {
+		return postedOn;
+	}
+}
